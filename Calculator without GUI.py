8f030f3e1a91f2efd034f1c119b34309d2b6c0f1
Dragan Mirilo <draganmirilo86@gmail.com>
2023-01-24 20:00:03 +0100

    Calculator

--- Calculator without GUI.py
+++ Calculator without GUI.py
@@ -5,41 +5,48 @@
 
 
 import math
+import os
+from snakecase import snakecase
 
-def calculator():
-    while True: 
-        # Defining variables
-        result = 0
+for filename in os.listdir():
+    os.rename(filename, snakecase(filename))
 
-        # Input by user
-        entry = input("Enter operand ( +, -, *, /, sqrt, exp, log ): ")
-   
-        # if the "end" is entered, stop the loop
-        if entry == 'End':
-            break
-   
-        # ask number from user
-        num1 = float(input("Enter number 1: "))
-        num1 = float(input("Enter number 2: "))
-    
-        # calculate the result
-        if entry == '+':
-            result = num1 + num2
-        elif entry == '-':
-            result = num1 - num2
-        elif entry == '*':
-            result = num1 * num2
-        elif entry == '/':
-            result = num1 / num2
-        elif entry == 'sqrt':
-            result = math.sqrt(num1,)
-        elif entry == 'exp':
-            result = math.exp(num1,)
-        elif entry == 'log':
-            result = math.exp(num1,)
-    
-        # results displays
-        print("Current result: ")
+class Calculator:
+
+    def calculator():
+        while True: 
+            # Defining variables
+            result = 0
+
+            # Input by user
+            entry = input("Enter operand ( +, -, *, /, sqrt, exp, log ): ")
+
+            # if the "end" is entered, stop the loop
+            if entry == 'End':
+                break
+
+            # ask number from user
+            num1 = float(input("Enter number 1: "))
+            num1 = float(input("Enter number 2: "))
+
+            # calculate the result
+            if entry == '+':
+                result = num1 + num2
+            elif entry == '-':
+                result = num1 - num2
+            elif entry == '*':
+                result = num1 * num2
+            elif entry == '/':
+                result = num1 / num2
+            elif entry == 'sqrt':
+                result = math.sqrt(num1,)
+            elif entry == 'exp':
+                result = math.exp(num1,)
+            elif entry == 'log':
+                result = math.exp(num1,)
+
+            # results displays
+            print("Current result: ")
 
 
 # In[ ]:
